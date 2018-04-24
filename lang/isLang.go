960d0e625e59a61af6a8968b8240a50451feed53
notechsolution <lezhi.zeng@163.com>
2018-04-24 23:19:52 +0800

    [Lang] add isNumber func

--- lang/isLang.go
+++ lang/isLang.go
@@ -3,7 +3,7 @@ package lo
 // Check whether the input string is number or not. using the ascii Dec char value to check.
 func IsNumber(number string) bool {
 	for _, c := range number {
-		if c < 48 || c > 57 {
+		if c < '0' || c > '9' {
 			return false;
 		}
 	}
